7563bf682b912737c75e87dd4d10f4c5a34f25cb
Vikash Kumar <vikashjjp728@gmail.com>
2025-09-14 08:12:53 +0530

    Solve new problem on 14/09/2025

---
+++ A_Colorful_Stones__Simplified_Edition.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    int pos = 1;
+    for (char c : t)
+    {
+        if (s[pos - 1] == c)
+        {
+            pos++;
+        }
+    }
+    cout << pos << endl;
+    return 0;
+}
---
+++ A_Turtle_Puzzle_Rearrange_and_Negate.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += abs(a[i]);
+        }
+        cout << sum << endl;
+    }
+
+    return 0;
+}
