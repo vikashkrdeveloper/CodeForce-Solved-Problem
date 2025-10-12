a2a18576f8a9bb38ab7e1415467cec85783b51f6
VIKASH KUMAR <128849038+vikashkrdeveloper@users.noreply.github.com>
2025-10-12 07:18:46 +0530

    Add comments to ios sync and cin tie calls

--- A_Coins.cpp
+++ A_Coins.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    ios::sync_with_stdio(0); // ==> 
+    cin.tie(0); // ==> 
     int t;
     cin >> t;
     while (t--)
