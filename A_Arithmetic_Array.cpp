ef5dcb53f486e1da6d630e1b1d794c018ed31a46
VIKASH KUMAR <128849038+vikashkrdeveloper@users.noreply.github.com>
2025-10-14 11:11:06 +0530

    Update A_Arithmetic_Array.cpp

--- A_Arithmetic_Array.cpp
+++ A_Arithmetic_Array.cpp
@@ -5,8 +5,8 @@ int main() // ==>
 { // ==> 
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int t;
-    cin >> t;
+    int t; // ==> 
+    cin >> t; // ==> 
     while (t--)
     {
         int n;
